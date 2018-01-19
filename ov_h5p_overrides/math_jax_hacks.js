f2923342a408e6bd3c8e65fd02acac38b6850894
Karli Raudsepp <karliraudsepp@hazarada.htk.tlu.ee>
2018-01-19 16:14:21 +0200

    references #57

--- ov_h5p_overrides/math_jax_hacks.js
+++ ov_h5p_overrides/math_jax_hacks.js
@@ -1,10 +1,17 @@
 addEventListener("load", function() {
 
-	var nav_tabs = document.querySelectorAll("nav ol li a");
-	for (var i = 0; i < nav_tabs.length; i++) {
-		nav_tabs[i].addEventListener("click", function() {
-			MathJax.Hub.Typeset();
-		});
+	function add_math_jax_triggers(selector) {
+		var els = document.querySelectorAll(selector);
+		for (var i = 0; i < els.length; i++) {
+			console.log(els[i]);
+			els[i].addEventListener("click", function() {
+				MathJax.Hub.Typeset();
+			});
+		}
 	}
 
+	add_math_jax_triggers("nav ol li a");
+	add_math_jax_triggers(".h5p-footer-next-slide");
+	add_math_jax_triggers(".h5p-footer-previous-slide");
+	add_math_jax_triggers(".h5p-image-hotspot");
 });
