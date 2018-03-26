c175b57687a087439e020fb1a3f6b674866fe494
Pjotr Savitski <gnum@tlu.ee>
2018-03-26 15:21:59 +0300

    Added check for externalEventDispatcher to be present.
    H5P seems to only have jQuery in context of libraries list.

--- ov_analytics/js/ov_analytics_h5p.js
+++ ov_analytics/js/ov_analytics_h5p.js
@@ -1,7 +1,7 @@
 (function ($) {
   Drupal.behaviors.ovAnalytics = {
     attach: function (context, settings) {
-      if ( window.H5P )
+      if ( window.H5P && window.H5P.externalDispatcher )
       {
         var moduleSettings = settings.ovAnalytics;
         H5P.externalDispatcher.on('xAPI', function (event) {
