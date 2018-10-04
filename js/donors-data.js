62032dff6b535c0b73c0bf9e6b8c8ee28c34b5a3
Pjotr Savitski <gnum@tlu.ee>
2018-10-04 15:59:57 +0300

    Resolves #115, changed donors visuals, also fixed actions box height.
    Added a new logic to deal with donors visuals (hidden by default and
    shown after clicking on action image). Also added visual fixes for both
    Challenge and Donors, provided that one of those is being used.

--- js/donors-data.js
+++ js/donors-data.js
@@ -6,14 +6,58 @@
 
   H5P.jQuery('document').ready(function() {
     if (H5P.instances && H5P.instances.length > 0) {
+      /**
+      * Helper for creating action bar buttons.
+      *
+      * @private
+      * @param {string} type
+      * @param {string} customClass Instead of type class
+      */
+      var addActionBarButton = function (instance, $element, type, customClass) {
+        // Apply styling fixes to h5p-actions element, allow multiple lines
+        $element.css('box-sizing', 'content-box').css('height', 'auto').css('display', 'flow-root');
+        /**
+        * Handles selection of action
+        */
+        var handler = function () {
+          instance.trigger(type);
+        };
+        H5P.jQuery('<li/>', {
+          class: 'h5p-button h5p-noselect h5p-' + (customClass ? customClass : type),
+          style: 'float:right;',
+          role: 'button',
+          tabindex: 0,
+          title: 'Meede',
+          html: '<img src="' + H5PIntegration.baseUrl + '/sites/all/modules/oppevara/images/donors-logo-16px.jpg" alt="donors-logo-16px" style="vertical-align:middle;">',
+          on: {
+            click: handler,
+            keypress: function (e) {
+              if (e.which === 32) {
+                handler();
+                e.preventDefault(); // (since return false will block other inputs)
+              }
+            }
+          },
+          appendTo: $element
+        });
+      };
+
       H5P.jQuery.each(H5P.instances, function(index, instance) {
         var $container = H5P.jQuery('.h5p-content[data-content-id="' + instance.contentId + '"]').get(0);
         var $actions = H5P.jQuery($container).find('.h5p-actions');
+
+        addActionBarButton(instance, $actions, 'donors');
+        instance.on('donors', function() {
+          H5P.jQuery($container).find('.ov-donors-data').toggle(1, function() {
+            H5P.trigger(instance, 'resize');
+          });
+        });
+
         H5P.jQuery('<div/>', {
           class: 'ov-donors-data',
           style: 'font-size: 12px;font-family: Sans-Serif;color: #999;display:table;margin:0.5em;',
           html: '<img src="' + H5PIntegration.baseUrl + '/sites/all/modules/oppevara/images/donors-logo.jpg" alt="donors-logo" style="width:150px;height:80px;display:table-cell;margin-right:1em;"><span style="display:table-cell;vertical-align:top;">Digiõppematerjalid on valminud Euroopa Sotsiaalfondi meetme „Kaasaegse ja uuendusliku õppevara arendamine ja kasutuselevõtt“ raames Haridus- ja Teadusministeeriumi tellimusel.</span>'
-        }).insertAfter($actions);
+        }).insertAfter($actions).hide();
         H5P.jQuery('<div/>', {
           class: 'ov-creative-commons',
           style: 'text-align:center;font-family: Sans-Serif;font-size:12px;',
