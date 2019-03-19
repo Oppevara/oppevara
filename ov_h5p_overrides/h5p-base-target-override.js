c4163273fdc44f8262766df8daeeb431bdde9518
Sander A <aido@tlu.ee>
2019-03-19 16:09:01 +0200

    Changed base target to '_blank' on every embed
    Fixes #119

---
+++ ov_h5p_overrides/h5p-base-target-override.js
@@ -0,0 +1,8 @@
+(function ($) {
+    var $base = $("base");
+    if($base.length > 0){
+        $base.attr("target", "_blank");
+    } else {
+        $("head").prepend("<base target='_blank'>");
+    }
+})(H5P.jQuery);
