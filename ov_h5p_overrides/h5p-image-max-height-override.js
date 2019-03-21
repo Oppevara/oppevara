0976c115746d325f6e5ddd6c2bc4f24331b5cfcd
Sander A <aido@tlu.ee>
2019-03-21 18:41:32 +0200

    Added dynamic max-height to images in column
    Calculate viewport height and width on each resize and apply max-height and max-width to images.
    
    ref #123

---
+++ ov_h5p_overrides/h5p-image-max-height-override.js
@@ -0,0 +1,23 @@
+(function ($) {
+
+    var h5pImgSelector = '.h5p-column-content.h5p-image > img';
+
+    $('head').append('<style id="h5pImageMaxDimensions" type="text/css">' + h5pImgSelector + ' {max-height: 100%; max-width: 100%;}</style>');
+
+    setMaxDimensions();
+    function setMaxDimensions(){
+        var vh = window.top.innerHeight - 50; // height of the parent frame (viewport) minus a little padding for possible menus etc.
+        var vw = window.innerWidth; // width of the iframe iself
+
+        $('#h5pImageMaxDimensions').text(h5pImgSelector + ' {max-height: ' + vh + 'px; max-width: ' + vw +'px; }');
+    }
+
+    var resizeTimer;
+    $(window).resize(function() {
+        // timeout to debounce resizing, so it won't fire too often WHILE resizing
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(function() {
+            setMaxDimensions();
+        }, 100);
+    });
+})(H5P.jQuery);
