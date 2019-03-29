b055c75fac6bb8ad443d8e93785866f4853ae020
Sander A <aido@tlu.ee>
2019-03-29 14:08:01 +0200

    Prevent jumping on iOS
    When menu bar appears and dissapears

--- ov_h5p_overrides/h5p-image-max-height-override.js
+++ ov_h5p_overrides/h5p-image-max-height-override.js
@@ -1,6 +1,7 @@
 (function ($) {
 
     var h5pImgSelector = '.h5p-column-content.h5p-image > img';
+    var previousHeight = 0;
 
     $('head').append('<style id="h5pImageMaxDimensions" type="text/css">' + h5pImgSelector + ' {max-height: 100%; max-width: 100%;}</style>');
 
@@ -9,7 +10,10 @@
         var vh = window.top.innerHeight - 50; // height of the parent frame (viewport) minus a little padding for possible menus etc.
         var vw = window.innerWidth; // width of the iframe iself
 
-        $('#h5pImageMaxDimensions').text(h5pImgSelector + ' {max-height: ' + vh + 'px; max-width: ' + vw +'px; }');
+        if(vh-previousHeight > 100){ // for iOS and browers jumping menu bars
+            $('#h5pImageMaxDimensions').text(h5pImgSelector + ' {max-height: ' + vh + 'px; max-width: ' + vw +'px; }');
+            previousHeight = vh;
+        }
     }
 
     var resizeTimer;
@@ -18,7 +22,7 @@
         clearTimeout(resizeTimer);
         resizeTimer = setTimeout(function() {
             setMaxDimensions();
-            H5P.trigger(H5P.instances[0], 'resize');
+            //H5P.trigger(H5P.instances[0], 'resize');
         }, 100);
     });
 })(H5P.jQuery);
