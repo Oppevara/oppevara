4705ef079ab66f86ddfd9108dc5b2c52cc64cd69
Sander A <aido@tlu.ee>
2019-03-29 14:44:21 +0200

    Removed bounce-check for live-tesing

--- ov_h5p_overrides/h5p-image-max-height-override.js
+++ ov_h5p_overrides/h5p-image-max-height-override.js
@@ -1,7 +1,7 @@
 (function ($) {
 
     var h5pImgSelector = '.h5p-column-content.h5p-image > img';
-    var previousHeight = 0;
+    var previousHeight;
 
     $('head').append('<style id="h5pImageMaxDimensions" type="text/css">' + h5pImgSelector + ' {max-height: 100%; max-width: 100%;}</style>');
 
@@ -10,10 +10,7 @@
         var vh = window.top.innerHeight - 50; // height of the parent frame (viewport) minus a little padding for possible menus etc.
         var vw = window.innerWidth; // width of the iframe iself
 
-        if(vh-previousHeight > 100){ // for iOS and browers jumping menu bars
-            $('#h5pImageMaxDimensions').text(h5pImgSelector + ' {max-height: ' + vh + 'px; max-width: ' + vw +'px; }');
-            previousHeight = vh;
-        }
+        $('#h5pImageMaxDimensions').text(h5pImgSelector + ' {max-height: ' + vh + 'px; max-width: ' + vw +'px; }');
     }
 
     var resizeTimer;
